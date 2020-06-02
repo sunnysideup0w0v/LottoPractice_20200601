eef0ffcdeaf9b48a415872dcdf7a3c28365abe8d
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-06-02 09:36:31 +0900

    Initial commit

---
+++ app/src/main/java/com/example/lottopractice_20200601/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.example.lottopractice_20200601;
+
+import android.content.Context;
+
+public abstract class BaseActivity {
+    public abstract void setupEvents();
+    public abstract void setValues();
+
+}
